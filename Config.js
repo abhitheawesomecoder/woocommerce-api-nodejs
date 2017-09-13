7e4a8dcd63b12aee5313e49a5cb410563201dc83
Abhishek Kumar <abhitheawesomecoder@gmail.com>
2017-09-13 14:59:08 +0530

    first commit

---
+++ Config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Constants = {
+    URL: {
+        root: 'http://test.com'
+    },
+    Keys: {
+	    ConsumeKey: 'ck_ade1b054013bf695ffef1a897504df7f27ec1738',
+	    ConsumerSecret: 'cs_7764a7465037bfc0c7b1d999ea8ecccbe384e8db'
+    }
+}
+
+module.exports = Constants;
